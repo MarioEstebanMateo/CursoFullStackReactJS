e91ef1106af43fd095278bf87be3a2f1ea998ed0
Mario Esteban Mateo <memateo@gmail.com>
2021-09-12 23:37:56 -0300

    target:"_blank" en links de footer

--- Modulo 4/Unidad 1/Libreria El Socorro/frontend/src/components/layout/Footer.js
+++ Modulo 4/Unidad 1/Libreria El Socorro/frontend/src/components/layout/Footer.js
@@ -7,10 +7,10 @@ const Footer = (props) => {
         <footer>    
             <div className="footer1">
                 <h4>Seguinos en nuestras redes!</h4>
-                <a className="boton_redes" href="https://www.instagram.com/libreriaelsocorro" role="button"><i className="fab fa-instagram"></i></a>
+                <a className="boton_redes" href="https://www.instagram.com/libreriaelsocorro" role="button"><i className="fab fa-instagram" target="_blank"></i></a>
                 <a className="boton_redes" href="https://www.facebook.com/Libreria-El-Socorro-101095425635524" role="button"><i
-                        className="fab fa-facebook-f"></i></a>
-                <a className="boton_redes" href="https://twitter.com/libreriaelsoco1" role="button"><i className="fab fa-twitter"></i></a>
+                        className="fab fa-facebook-f" target="_blank"></i></a>
+                <a className="boton_redes" href="https://twitter.com/libreriaelsoco1" role="button"><i className="fab fa-twitter" target="_blank"></i></a>
             </div>
             <div className="footer2">
                 © 2021 Copyright: <a class="link_developer" href="index.html">Diseño de Mario Esteban Mateo para Diplomatura Full Stack con React - UTN</a>
